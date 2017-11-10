e8cd9618e4d18c0ebbff800c417e1193e638ea6a
kokeneko <33151121+kokeneko@users.noreply.github.com>
2017-11-10 20:03:23 +0900

    問題4 if

---
+++ kadai4/src/kadai4/Main.java
@@ -0,0 +1,17 @@
+package kadai4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 3;
+		
+		if(a % 2 == 0) {
+			System.out.println("偶数");
+		}
+		else {
+			System.out.println("奇数");
+		}
+
+	}
+
+}
