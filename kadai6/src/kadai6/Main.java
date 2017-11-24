3a8eccc12d0f0008062ca47e0b0916c0422a18fa
kokeneko <33151121+kokeneko@users.noreply.github.com>
2017-11-24 20:40:07 +0900

    我%d使いたい

---
+++ kadai6/src/kadai6/Main.java
@@ -0,0 +1,13 @@
+package kadai6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+		for(int i = 0;i < 10;i++) {
+			System.out.println("a[" + i + "] = " + a[i]);
+		}
+	}
+
+}
