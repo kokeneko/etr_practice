49d6edcd70c5ac944ae230e19ea665e8a9cfe4cc
kokeneko <33151121+kokeneko@users.noreply.github.com>
2017-12-03 21:24:56 +0900

    ETR12/1分

---
+++ sample1201/src/sample1201/Hoge.java
@@ -0,0 +1,10 @@
+package sample1201;
+
+public class Hoge {
+	private static int score;
+
+	public void hoge() {
+		score = 100;
+		System.out.println(score);
+	}
+}
---
+++ sample1201/src/sample1201/Main.java
@@ -0,0 +1,11 @@
+package sample1201;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Hoge hoge = new Hoge();
+		hoge.hoge();
+
+	}
+
+}
